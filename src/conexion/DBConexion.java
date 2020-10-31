7c237b46a09f230e20c049543c648b16ebcb62ba
HugoCobon <hugocobon30@gmail.com>
2020-10-31 03:46:33 -0600

    Entregables Del Proyecto

--- src/conexion/DBConexion.java
+++ src/conexion/DBConexion.java
@@ -1,13 +1,10 @@
-
 package conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-
 public class DBConexion {
-    
-    
+
     private final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private final String PASSWORD = "12345678abc";
     private final String USUARIO = "root";
@@ -22,22 +19,22 @@ public class DBConexion {
     public Connection conectarme() {
         try {
             Class.forName(DRIVER);
-            this.cadena= DriverManager.getConnection(URL, USUARIO, PASSWORD);
-            
+            this.cadena = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        
-        
+
         return this.cadena;
     }
-public void desconectar(){
-    try {
-        this.cadena.close();
-    } catch (Exception e) {
-        System.out.println(e.getMessage());
+
+    public void desconectar() {
+        try {
+            this.cadena.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
-    
-}
-    
+
 }
